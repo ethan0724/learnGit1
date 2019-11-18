16c1f2c3900bdb109ba23ceb79238e123a2b42d5
ethan0724 <ethan0724@hanmail.net>
2019-11-18 21:30:07 +0900

    add func print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+import sys
+
 def print_messages():
     messages = "world"
     print("hello {}".format(messages))
+
+def print_git():
+    messeage = "from the hell"
+    sys.print
 
 if__name == "__main__":
     print_messages()
